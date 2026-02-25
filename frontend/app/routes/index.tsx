0e3873bc90541c94a3c71583328a1fbad9efebc5
Opjasson <azmitheusos333@gmail.com>
2026-02-26 00:01:54 +0700

    frontend : first create folder routes and pages

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -4,5 +4,11 @@ import { createStackNavigator } from "@react-navigation/stack";
 const Route = () => {
     const Stack = createStackNavigator();
 
-    
-}
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="login" component={} />
+        </Stack.Navigator>
+    );
+};
+
+export default Route;
